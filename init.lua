1bd5e81150a503ff65c0c12479d2a44e26319a7c
Steven-Whitehead <Steven.Whitehead@ibm.com>
2025-08-13 08:45:50 -0400

    refactors

--- init.lua
+++ init.lua
@@ -1,9 +1,4 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
 
--- Install package manager
---    https://github.com/folke/lazy.nvim
---    `:help lazy.nvim.txt` for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
@@ -18,24 +13,18 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-  -- NOTE: First, some plugins that don't require any configuration
-
-  -- Git related plugins
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
   'tpope/vim-commentary',
-
-  -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
-
   'eapache/rainbow_parentheses.vim',
   'wellsjo/vim-save-cursor-position',
   'thinca/vim-visualstar',
   'Shougo/echodoc.vim',
   'fatih/vim-go',
-  -- 'chrisbra/Colorizer',
   'hashivim/vim-terraform',
 
+  -- TODO am I using? if not, remove
   {
      "L3MON4D3/LuaSnip",
      -- follow latest release.
@@ -44,26 +33,7 @@ require('lazy').setup({
      build = "make install_jsregexp"
   },
 
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    lazy = false, -- neo-tree will lazily load itself
-    -- -@module "neo-tree"
-    -- -@type neotree.Config?
-    opts = {
-      -- fill any relevant options here
-    },
-  },
-
-  "mrbjarksen/neo-tree-diagnostics.nvim",
-
-  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'}, -- TODO seems to not be supported
   {'neovim/nvim-lspconfig'},
   {'hrsh7th/cmp-nvim-lsp'},
   {'hrsh7th/nvim-cmp'},
@@ -78,11 +48,9 @@ require('lazy').setup({
     },
   },
 
-  -- Adds git related signs to the gutter, as well as utilities for managing changes
   {
     'lewis6991/gitsigns.nvim',
     opts = {
-      -- See `:help gitsigns.txt`
       signs = {
         add = { text = '+' },
         change = { text = '~' },
@@ -91,7 +59,7 @@ require('lazy').setup({
         changedelete = { text = '~' },
       },
       on_attach = function(bufnr)
-        vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
+        -- vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
 
         -- don't override the built-in and fugitive keymaps
         local gs = package.loaded.gitsigns
@@ -118,12 +86,10 @@ require('lazy').setup({
   },
 
   {
-    -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
     opts = {
       options = {
-        icons_enabled = false,
+        icons_enabled = true,
         -- theme = 'onedark',
         component_separators = '|',
         section_separators = '',
@@ -143,13 +109,8 @@ require('lazy').setup({
     branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-      -- Only load if `make` is available. Make sure you have the system
-      -- requirements installed.
       {
         'nvim-telescope/telescope-fzf-native.nvim',
-        -- NOTE: If you are having trouble with this installation,
-        --       refer to the README for telescope-fzf-native for more instructions.
         build = 'make',
         cond = function()
           return vim.fn.executable 'make' == 1
@@ -172,45 +133,84 @@ require('lazy').setup({
 
 }, {})
 
--- [[ Setting options ]]
--- Set highlight on search
--- vim.o.hlsearch = false
-
--- Make line numbers default
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 vim.wo.number = true
-
--- Enable mouse mode
 vim.o.mouse = 'a'
 vim.o.shortmess = 'a'
-
--- Sync clipboard between OS and Neovim.
 vim.o.clipboard = 'unnamedplus'
-
--- Enable break indent
--- vim.o.breakindent = true
-
--- Save undo history
 vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or capital in search
 vim.o.ignorecase = true
 vim.o.smartcase = true
-
--- Keep signcolumn on by default
 vim.wo.signcolumn = 'yes'
-
--- Decrease update time
 vim.o.updatetime = 250
 -- vim.o.timeoutlen = 300
-
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
-
--- NOTE: You should make sure your terminal supports this
+vim.o.completeopt = 'fuzzy,menu,menuone,noinsert,popup,preview'
 vim.o.termguicolors = true
 
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+-- vim.opt.showbreak = "↪ "  -- You can customize this symbol
+vim.opt.colorcolumn = "120"
+vim.opt.inccommand = "split"
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 3
+
+-- Terminal scrollback buffer size
+vim.g.terminal_scrollback_buffer_size = 10000
+
+-- GUI cursor settings
+vim.opt.guicursor = {
+  "n-v-c:block",
+  "i-ci-ve:ver25",
+  "r-cr:hor20",
+  "o:hor50",
+  "a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor",
+  "sm:block-blinkwait175-blinkoff150-blinkon175"
+}
+
+-- Better display for messages
+vim.opt.cmdheight = 3
+vim.opt.shortmess:append("c")
+
+-- Go plugin settings
+vim.g.go_def_mapping_enabled = 0
+-- vim.g.go_list_type = "quickfix" -- Uncomment if needed
+vim.g.go_highlight_build_constraints = 1
+vim.g.go_highlight_extra_types = 1
+vim.g.go_highlight_fields = 1
+vim.g.go_highlight_functions = 1
+vim.g.go_highlight_function_calls = 1
+vim.g.go_highlight_methods = 1
+vim.g.go_highlight_operators = 1
+vim.g.go_highlight_structs = 1
+vim.g.go_highlight_types = 1
+
+-- Rainbow Parentheses
+vim.g.rbpt_colorpairs = {
+  {"brown", "SeaGreen3"},
+  {"gray", "firebrick3"},
+  {"cyan", "DarkOrchid3"},
+}
+
+-- Split behavior
+vim.opt.splitbelow = true  -- New horizontal splits open below
+vim.opt.splitright = true  -- New vertical splits open to the right
+
+-- Fill characters for window separators
+vim.opt.fillchars:append({ vert = "│" })
+
+-- Environment variable
+vim.env.GINKGO_EDITOR_INTEGRATION = "true"
+
+-- Highlight trailing whitespace
+vim.cmd([[highlight ExtraWhitespace ctermbg=red guibg=red]])
+vim.cmd([[match ExtraWhitespace /\s\+$/]])
+
 -- [[ Basic Keymaps ]]
--- vim.keymap.set('n', '<leader>e', ':Explore<CR>')
+vim.keymap.set('n', '<leader>e', ':Explore<CR>')
 vim.keymap.set('n', '<Leader>v', ':vsplit<CR>')
 vim.keymap.set('n', '<Leader>s', ':split<CR>')
 vim.keymap.set('n', '<Leader>c', ':set cursorline! cursorcolumn!<CR>')
@@ -220,15 +220,63 @@ vim.keymap.set('n', '<Leader>g', ':tabnext<CR>')
 vim.keymap.set('n', '<Leader>i', ':set foldmethod=indent<CR>')
 -- vim.keymap.set('n', '<silent> gi', ':GoInfo<CR>')
 vim.keymap.set('n', '<Leader>r', ':GoReferrers<CR>')
-
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+-- vim.keymap.set('n', '<leader>/', function()
+--   -- You can pass additional configuration to telescope to change theme, layout, etc.
+--   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+--     winblend = 10,
+--     previewer = false,
+--   })
+-- end, { desc = '[/] Fuzzily search in current buffer' })
 
--- Remap for dealing with word wrap
--- vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
--- vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
 
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]])
+
+vim.keymap.set('t', '<A-h>', [[<C-\><C-N><C-w>h]])
+vim.keymap.set('t', '<A-j>', [[<C-\><C-N><C-w>j]])
+vim.keymap.set('t', '<A-k>', [[<C-\><C-N><C-w>k]])
+vim.keymap.set('t', '<A-l>', [[<C-\><C-N><C-w>l]])
+
+vim.keymap.set('i', '<A-h>', [[<C-\><C-N><C-w>h]])
+vim.keymap.set('i', '<A-j>', [[<C-\><C-N><C-w>j]])
+vim.keymap.set('i', '<A-k>', [[<C-\><C-N><C-w>k]])
+vim.keymap.set('i', '<A-l>', [[<C-\><C-N><C-w>l]])
+
+vim.keymap.set('n', '<A-h>', [[<C-w>h]])
+vim.keymap.set('n', '<A-j>', [[<C-w>j]])
+vim.keymap.set('n', '<A-k>', [[<C-w>k]])
+vim.keymap.set('n', '<A-l>', [[<C-w>l]])
+vim.api.nvim_set_keymap("n", "gi", ":GoInfo<CR>", { noremap = true, silent = true })
+
+-- Key mapping for file explorer
+vim.keymap.set("n", "<leader>e", ":Explore<CR>", { noremap = true, silent = true })
+
+
+-- document existing key chains
+-- require('which-key').register {
+--   ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+--   ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+--   ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+--   ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
+--   ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+--   ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+--   ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+-- }
+
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
@@ -254,24 +302,6 @@ require('telescope').setup {
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
 
--- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
--- vim.keymap.set('n', '<leader>/', function()
---   -- You can pass additional configuration to telescope to change theme, layout, etc.
---   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
---     winblend = 10,
---     previewer = false,
---   })
--- end, { desc = '[/] Fuzzily search in current buffer' })
-
-vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
--- vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
--- vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
--- vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
--- vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
--- vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
--- vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
@@ -284,8 +314,6 @@ vim.defer_fn(function()
       -- 'go',
       'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash',
     },
-
-    -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = true,
 
 --     highlight = { enable = true },
@@ -346,11 +374,6 @@ vim.defer_fn(function()
   }
 end, 0)
 
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
@@ -397,34 +420,7 @@ local on_attach = function(_, bufnr)
   end, { desc = 'Format current buffer with LSP' })
 end
 
--- mappings for terminal mode
-vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]])
 
-vim.keymap.set('t', '<A-h>', [[<C-\><C-N><C-w>h]])
-vim.keymap.set('t', '<A-j>', [[<C-\><C-N><C-w>j]])
-vim.keymap.set('t', '<A-k>', [[<C-\><C-N><C-w>k]])
-vim.keymap.set('t', '<A-l>', [[<C-\><C-N><C-w>l]])
-
-vim.keymap.set('i', '<A-h>', [[<C-\><C-N><C-w>h]])
-vim.keymap.set('i', '<A-j>', [[<C-\><C-N><C-w>j]])
-vim.keymap.set('i', '<A-k>', [[<C-\><C-N><C-w>k]])
-vim.keymap.set('i', '<A-l>', [[<C-\><C-N><C-w>l]])
-
-vim.keymap.set('n', '<A-h>', [[<C-w>h]])
-vim.keymap.set('n', '<A-j>', [[<C-w>j]])
-vim.keymap.set('n', '<A-k>', [[<C-w>k]])
-vim.keymap.set('n', '<A-l>', [[<C-w>l]])
-
--- document existing key chains
--- require('which-key').register {
---   ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
---   ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
---   ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
---   ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
---   ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
---   ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
---   ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
--- }
 
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
@@ -440,10 +436,8 @@ vim.keymap.set('n', '<A-l>', [[<C-w>l]])
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  -- clangd = {},
   -- gopls = {},
   -- pyright = {},
-  -- rust_analyzer = {},
   -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
 
@@ -549,108 +543,9 @@ require'lspconfig'.jdtls.setup{}
 
 vim.cmd("colorscheme sorbet")
 
-require("neo-tree").setup({
-  sources = {
-    "filesystem",
-    "buffers",
-    "git_status",
-    "diagnostics",
-    -- ...and any additional source
-  },
-  -- These are the defaults
-  diagnostics = {
-    auto_preview = { -- May also be set to `true` or `false`
-      enabled = false, -- Whether to automatically enable preview mode
-      preview_config = {}, -- Config table to pass to auto preview (for example `{ use_float = true }`)
-      event = "neo_tree_buffer_enter", -- The event to enable auto preview upon (for example `"neo_tree_window_after_open"`)
-    },
-    bind_to_cwd = true,
-    diag_sort_function = "severity", -- "severity" means diagnostic items are sorted by severity in addition to their positions.
-                                     -- "position" means diagnostic items are sorted strictly by their positions.
-                                     -- May also be a function.
-    follow_current_file = { -- May also be set to `true` or `false`
-      enabled = true, -- This will find and focus the file in the active buffer every time
-      always_focus_file = false, -- Focus the followed file, even when focus is currently on a diagnostic item belonging to that file
-      expand_followed = true, -- Ensure the node of the followed file is expanded
-      leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
-      leave_files_open = false, -- `false` closes auto expanded files, such as with `:Neotree reveal`
-    },
-    group_dirs_and_files = true, -- when true, empty folders and files will be grouped together
-    group_empty_dirs = true, -- when true, empty directories will be grouped together
-    show_unloaded = true, -- show diagnostics from unloaded buffers
-    refresh = {
-      delay = 100, -- Time (in ms) to wait before updating diagnostics. Might resolve some issues with Neovim hanging.
-      event = "vim_diagnostic_changed", -- Event to use for updating diagnostics (for example `"neo_tree_buffer_enter"`)
-                                        -- Set to `false` or `"none"` to disable automatic refreshing
-      max_items = 10000, -- The maximum number of diagnostic items to attempt processing
-                         -- Set to `false` for no maximum
-    },
-  },
-})
 
 
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    -- Open NeoTree when Neovim starts
-    vim.cmd("Neotree")
-    vim.cmd("Neotree diagnostics bottom")
-  end,
-})
 
--- Appearance
-vim.opt.linebreak = true
-vim.opt.breakindent = true
--- vim.opt.showbreak = "↪ "  -- You can customize this symbol
-
-vim.opt.colorcolumn = "120"
-
--- As-you-go substitution
-vim.opt.inccommand = "split"
-
--- Key mapping
-vim.api.nvim_set_keymap("n", "gi", ":GoInfo<CR>", { noremap = true, silent = true })
-
--- Indentation
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 3
-
--- Terminal scrollback buffer size
-vim.g.terminal_scrollback_buffer_size = 10000
-
--- GUI cursor settings
-vim.opt.guicursor = {
-  "n-v-c:block",
-  "i-ci-ve:ver25",
-  "r-cr:hor20",
-  "o:hor50",
-  "a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor",
-  "sm:block-blinkwait175-blinkoff150-blinkon175"
-}
-
--- Better display for messages
-vim.opt.cmdheight = 3
-vim.opt.shortmess:append("c")
-
--- Go plugin settings
-vim.g.go_def_mapping_enabled = 0
--- vim.g.go_list_type = "quickfix" -- Uncomment if needed
-vim.g.go_highlight_build_constraints = 1
-vim.g.go_highlight_extra_types = 1
-vim.g.go_highlight_fields = 1
-vim.g.go_highlight_functions = 1
-vim.g.go_highlight_function_calls = 1
-vim.g.go_highlight_methods = 1
-vim.g.go_highlight_operators = 1
-vim.g.go_highlight_structs = 1
-vim.g.go_highlight_types = 1
-
--- Rainbow Parentheses
-vim.g.rbpt_colorpairs = {
-  {"brown", "SeaGreen3"},
-  {"gray", "firebrick3"},
-  {"cyan", "DarkOrchid3"},
-}
 
 -- Autocommands for Rainbow Parentheses
 vim.api.nvim_create_autocmd("VimEnter", {
@@ -673,23 +568,3 @@ vim.api.nvim_create_autocmd("Syntax", {
   command = "RainbowParenthesesLoadBraces"
 })
 
--- Split behavior
-vim.opt.splitbelow = true  -- New horizontal splits open below
-vim.opt.splitright = true  -- New vertical splits open to the right
-
--- Fill characters for window separators
-vim.opt.fillchars:append({ vert = "│" })
-
--- Environment variable
-vim.env.GINKGO_EDITOR_INTEGRATION = "true"
-
--- Key mapping for file explorer
-vim.keymap.set("n", "<leader>e", ":Explore<CR>", { noremap = true, silent = true })
-
--- Highlight trailing whitespace
-vim.cmd([[highlight ExtraWhitespace ctermbg=red guibg=red]])
-vim.cmd([[match ExtraWhitespace /\s\+$/]])
-
--- load my old stuff
-local vimrc = vim.fn.stdpath("config") .. "/vimrc.vim"
-vim.cmd.source(vimrc)
